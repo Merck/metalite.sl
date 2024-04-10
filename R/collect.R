b467c28cdd6a1531310adc60e61b15ac28e98da6
niup0 <poyao.niu@merck.com>
2024-04-10 13:32:23 -0400

    create treatment compliance prepare function and bug fix.

--- R/collect.R
+++ R/collect.R
@@ -61,7 +61,7 @@ collect_baseline <- function(meta,
   par_var_group <- metalite::collect_adam_mapping(meta, parameter)$vargroup
 
   # Obtain Data
-  pop <- metalite::collect_population_record(meta, population, var = c(par_var))
+  pop <- metalite::collect_population_record(meta, population, var = c(par_var, par_var_group))
 
   # Obtain ID
   pop_id <- metalite::collect_adam_mapping(meta, population)$id
