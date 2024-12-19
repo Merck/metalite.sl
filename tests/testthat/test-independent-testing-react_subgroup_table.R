b0d5d557c422e80c615dde4d32850bb1c61942f8
lm3388 <lm3388@users.noreply.github.com>
2024-12-19 22:08:25 +0000

    Style code (GHA)

--- tests/testthat/test-independent-testing-react_subgroup_table.R
+++ tests/testthat/test-independent-testing-react_subgroup_table.R
@@ -1,13 +1,11 @@
-
 test_that("Testing react_subgroup_table function via calling react_base_char function", {
-  
   library(reactable)
   library(htmltools)
   if (!require("htmlwidgets", character.only = TRUE)) {
     install.packages("htmlwidgets")
     library(htmlwidgets)
   }
-  
+
   # the "react_subgroup_table()" function is called inside the "react_base_char()" function
   table_output <- react_base_char(
     metadata_sl = meta_sl_example(),
@@ -19,17 +17,15 @@ test_that("Testing react_subgroup_table function via calling react_base_char fun
     ae_subgroup = c("age", "race"),
     ae_specific = "rel",
     width = 1200
-    )
-  
-  
+  )
+
+
   # Define the file path for the HTML output
   file_path <- "react_subgroup_table.html"
-    
-  # Save the table output to the HTML file:  use saveWidget to save the reactable output  
-  saveWidget(table_output, file_path, selfcontained = TRUE) 
-  
+
+  # Save the table output to the HTML file:  use saveWidget to save the reactable output
+  saveWidget(table_output, file_path, selfcontained = TRUE)
+
   # Use expect_snapshot_file to compare the file against the snapshot
   expect_snapshot_file(file_path)
-    
-  
 })
