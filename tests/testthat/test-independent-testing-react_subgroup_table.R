75f40bf3837d36960d7446081289f475360ce3eb
Wang, Benjamin <benjamin.wang@merck.com>
2025-01-13 20:53:39 -0500

    use predictable dataKey

--- tests/testthat/test-independent-testing-react_subgroup_table.R
+++ tests/testthat/test-independent-testing-react_subgroup_table.R
@@ -1,6 +1,13 @@
 library(reactable)
 library(htmltools)
 
+expect_and_replace_dataKey <- function(tbl, newDataKey = "__predictable_data_key__") {
+  dataKey <- tbl$x$tag$attribs$dataKey
+  expect_true(is.character(dataKey) && nchar(dataKey) > 0)
+  tbl$x$tag$attribs$dataKey <- newDataKey
+  tbl
+}
+
 test_that("Testing react_subgroup_table function via calling react_base_char function", {
   # the "react_subgroup_table()" function is called inside the "react_base_char()" function
   table_output <- react_base_char(
@@ -14,7 +21,7 @@ test_that("Testing react_subgroup_table function via calling react_base_char fun
     ae_specific = "rel",
     width = 1200
   )
-
+  table_output <- expect_and_replace_dataKey(table_output)
   rendered <- htmltools::renderTags(table_output)
   expect_snapshot(cat(rendered$html))
 })
