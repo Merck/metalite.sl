0655924632182e3aaf2ae95c52c4e380801a526a
lm3388 <lm3388@users.noreply.github.com>
2025-03-25 17:26:13 +0000

    Style code (GHA)

--- tests/testthat/test-independent-testing-plotly_exp_duration.R
+++ tests/testthat/test-independent-testing-plotly_exp_duration.R
@@ -1,11 +1,10 @@
-
 # Prepare data
 meta <- meta_sl_exposure_example()
 
 outdata <- meta |> prepare_exp_duration()
 
 
-outdata_plot <- outdata|>
+outdata_plot <- outdata |>
   extend_exp_duration(
     duration_category_list = list(c(1, NA), c(7, NA), c(28, NA), c(12 * 7, NA), c(24 * 7, NA)),
     duration_category_labels = c(">=1 day", ">=7 days", ">=28 days", ">=12 weeks", ">=24 weeks")
@@ -14,18 +13,9 @@ outdata_plot <- outdata|>
 
 # Testing the plotly_exp_duration function - run multiple plots
 test_that("Interactive plot is created successfully", {
-  
   interactive_plot <- outdata_plot |> plotly_exp_duration(plot_type_label = c("Histogram", "Stacked histogram", "Horizontal histogram"))
-  
+
   expect_s3_class(interactive_plot, "shiny.tag.list")
-  
+
   expect_snapshot(interactive_plot)
 })
-
-
-
-
-
-
-
-
