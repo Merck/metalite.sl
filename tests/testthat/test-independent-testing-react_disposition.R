50122f2e3836a493f84d668cc36c6f5d78a4e437
Fukuda, Hiroaki <hiroaki.fukuda@merck.com>
2025-05-02 02:47:00 -0400

    Update `react_disposition()` to correctly extract subjects for a list

--- tests/testthat/test-independent-testing-react_disposition.R
+++ tests/testthat/test-independent-testing-react_disposition.R
@@ -3,9 +3,12 @@ library(htmltools)
 
 # Test 1 Overall testing for react_disposition function
 test_that("Testing react_disposition function", {
+  set.seed(123)
+  
   table <- react_disposition(
     metadata_sl = meta_sl_example(),
     metadata_ae = metalite.ae::meta_ae_example(),
+    trtvar = "trt01a",
     width = 1200
   )
 
@@ -21,9 +24,12 @@ test_that("Testing react_disposition function", {
 
 # Test 2 Testing AE selected column
 test_that("Testing react_disposition function AE selection column", {
+  set.seed(123)
+  
   table <- react_disposition(
     metadata_sl = meta_sl_example(),
     metadata_ae = metalite.ae::meta_ae_example(),
+    trtvar = "trt01a",
     ae_col_selected = c("AESOC", "ASTDT", "AENDT", "AETERM", "duration", "AESEV", "AESER", "related", "AEACN", "AEOUT"),
     ae_col_names = c("SOC", "Onset Date", "End Date", "AE", "Duraion", "Intensity", "Serious", "Related", "Action Taken", "Outcome"),
     width = 1200
@@ -41,9 +47,12 @@ test_that("Testing react_disposition function AE selection column", {
 
 # Test 3 Testing Disposition selected column
 test_that("Testing react_disposition function Disposition selection column", {
+  set.seed(123)
+  
   table <- react_disposition(
     metadata_sl = meta_sl_example(),
     metadata_ae = metalite.ae::meta_ae_example(),
+    trtvar = "trt01a",
     sl_col_selected = c("trt01a", "siteid", "subjid", "sex", "age", "weightbl"),
     sl_col_names = c("Treatment", "Site", "Subject ID", "Sex", "Age (Year)", "Weight (kg)"),
     width = 1200
